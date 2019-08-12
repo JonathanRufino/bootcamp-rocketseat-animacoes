eb90836ad30c48b380db349a54ea59c8822cd32a
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-08-12 20:30:45 -0300

    example: spring & decay

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Animated } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Animated } from "react-native";
 
 class App extends Component {
   state = {
-    ballY: new Animated.Value(0),
+    ballY: new Animated.Value(0)
   };
 
   componentDidMount() {
     const { ballY } = this.state;
 
-    Animated.timing(ballY, {
-      toValue: 500,
-      duration: 1000,
+    // Animated.spring(ballY, {
+    //   toValue: 300,
+    //   bounciness: 8 // default 8
+    // }).start();
+
+    Animated.decay(ballY, {
+      velocity: 0.5,
     }).start();
   }
 
@@ -29,15 +33,15 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 30,
+    padding: 30
   },
 
   ball: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    backgroundColor: '#f00',
-  },
+    backgroundColor: "#f00"
+  }
 });
 
 export default App;
